6de965aa44a7c00008383e986976c5fa916a447b
Rich Baird <rich@rbaird.me>
2023-03-13 17:39:33 -0600

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,49 @@
 #include <Arduino.h>
 
-#define CAPTURE_PIN 0 // TODO: change this to the correct pin
-#define ISR_PIN 0     // TODO: change this to the correct pin (this pin should be the pin that interrupt 2 is attached to on the arduino uno)
-#define CAPTURE_TIME 1000 // TODO: change this to the correct time (this is the time that the capture pin will be set to high for)
-#define CAPTURE_COMPLETE_CODE 0x00 // TODO: change this to the correct code (this is the code that will be sent to the client when the capture is complete)
-
+#define ISR_PIN                                                                \
+  0 // TODO: change this to the correct pin (this pin should be the pin that
+    // interrupt 2 is attached to on the arduino uno)
 char buffer[128];
 bool connected = false;
 
 /**
- * @brief This function will check to see if the client has requested a capture via serial
- * If the client has requested a capture, it will send a signal to the camera to
- * take a picture The signal will consist of a single pin being set to high for
- * a short period of time
+ * @brief This function will clear the buffer
+ * @param buffer The buffer to clear
+ * @param size The size of the buffer
+ */
+void clearBuffer(char *buffer, int size) {
+  for (int i = 0; i < size; i++) {
+    buffer[i] = 0;
+  }
+}
+
+/**
+ * @brief This function will check to see if the client has requested a capture
+ * via serial If the client has requested a capture, it will send a signal to
+ * the camera to take a picture The signal will consist of a single pin being
+ * set to high for a short period of time
  */
 void tryCapture() {
-  // TODO: implement this function
+  // clear the buffer
+  clearBuffer(buffer, 128);
+  // read the serial buffer until we get a null terminator or we have read 128 bytes
+  int bytesRead = Serial.readBytesUntil('\0', buffer, 128);
+  // if we have read more than 0 bytes and the buffer contains the word "capture"
+  if (bytesRead > 0 && strcmp(buffer, "capture") == 0) {
+    // send a signal to the camera to take a picture
+    digitalWrite(10, HIGH);
+    delay(100);
+    digitalWrite(10, LOW);
+  }
 }
 
 /**
  * @brief This function is attached to an external interrupt that is triggered
  * when the camera has finished capturing an image This function will send a
  * signal to the client that the image has been captured and downloaded. The
- * client will then be able to move to the next image
+ * client will then be able to move to the next image. The expected signal is the
+ * the word "next" sent over serial. This function is executed in an interrupt
+ * context so you won't find any references to it in the main loop.
  */
 void captureCompleteISR() {
   // TODO: implement this function
@@ -30,14 +51,15 @@ void captureCompleteISR() {
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(10, OUTPUT);
   Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(2), captureCompleteISR, RISING);
 }
 
 void loop() {
   // check for ping. This will let the client know that they are connected to
-  // the correct device. We read until we get a null terminator ('\0') because the
-  // client will send a string which is automatically null terminated.)
+  // the correct device. We read until we get a null terminator ('\0') because
+  // the client will send a string which is automatically null terminated.)
   if (!connected && Serial.readBytesUntil('\0', buffer, 128) > 0 &&
       strcmp(buffer, "ping") == 0) {
     Serial.println("PONG"); // send pong
